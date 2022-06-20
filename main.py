d4c706af3fec2bff2cfc7a9fe426dc004f9e1353
sujal03 <sanskarsujaltamrakar2903@gmail.com>
2022-06-20 17:52:47 +0530

    starting of the project...

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def printBoard():
+    pass
+
+if __name__ == "__main__":
+    xState =[0, 0, 0, 0, 0, 0, 0, 0]
+    zState =[0, 0, 0, 0, 0, 0, 0, 0]
+    turn = 1 # 1 for X and 0 for O
+    print("Welcome to Tic tac toe")
+    print("")
